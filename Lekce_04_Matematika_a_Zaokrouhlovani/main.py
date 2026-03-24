9fd585fad8479a29c66dee3680dc1ff84e06e04c
Librytir <lirytir@gymcheb.cz>
2026-03-24 08:31:47 +0100

    Update main.py

--- Lekce_04_Matematika_a_Zaokrouhlovani/main.py
+++ Lekce_04_Matematika_a_Zaokrouhlovani/main.py
@@ -1,15 +1,23 @@
 # ÚLOHA 1: Zvětšovač (použij +=)
 cislo = int(input("Zadej číslo: "))
-# Sem doplň kód:
+# Sem doplň kód:cislo = int(input("Zadej číslo: "))
+cislo += 10
+print("Výsledek:", cislo)
 
 
 # ÚLOHA 2: Útrata (zaokrouhli na 2 místa)
 celkem = float(input("Celková suma (Kč): "))
 lidi = int(input("Počet lidí: "))
-# Sem doplň výpočet a print s round():
+# Sem doplň výpočet a print s round():celkem = float(input("Celková suma (Kč): "))
+lidi = int(input("Počet lidí: "))
+na_osobu = celkem / lidi
+print("Každý zaplatí:", round(na_osobu, 2), "Kč")
+
 
 
 # ÚLOHA 3: Plocha kruhu (zaokrouhli na celé číslo)
 r = float(input("Zadej poloměr: "))
 # plocha = 3.14 * r * r
-# Sem doplň kód:
+# Sem doplň kód:r = float(input("Zadej poloměr: "))
+plocha = 3.14 * r * r
+print("Plocha kruhu:", round(plocha))
